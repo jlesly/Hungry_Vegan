f4ddd1f2ca4c21cfb4b44a0d99d6fd5625a2e3cd
Lesly <leslyj2@gmail.com>
2020-12-29 12:54:30 -0500

    continued to build cli class

--- lib/hungry_vegan/cli.rb
+++ lib/hungry_vegan/cli.rb
@@ -1,8 +1,9 @@
 module HungryVegan
     class Cli
-        attr_accessor = 
+        attr_accessor :restaurant_array, :zip_array, :zip
 
         def initialize
+            @zip_array = []
         end
 
         def call
@@ -18,21 +19,31 @@ module HungryVegan
             elsif 
                 input.length==5 && input.to_i !=0
                 @zip= input
-                
+                get_restaurants
+                show_restaurants
+                select_restaurant
+
             else
-                
+                invalid_entry
             end
 
         end 
 
-        def input 
-        end
-        
-        def invalid_entry
+        def get_restaurants
+            if @zip_array.include?(@zip)
+                @restaurant_array= Restaurant.get_matching_restaurats(@zip)    
+            else 
+                @zip_array<<@zip
+                @restaurant_array= Restaurant.get_restaurants_from_zip(@zip)
+            end
         end
 
-        def exit 
+        def show_restaurants 
         end 
+        
+        def invalid_entry
+            puts "\nInvalid entry.\n"
+        end
 
         def main_menu_option
         end 
