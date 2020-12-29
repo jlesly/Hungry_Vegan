7ff51a1b2ec0a4f3f5bd55b1264b8cd2154f1914
Lesly <leslyj2@gmail.com>
2020-12-29 11:15:48 -0500

    added requirements to file

--- lib/hungry_vegan.rb
+++ lib/hungry_vegan.rb
@@ -1,4 +1,12 @@
-require "hungry_vegan/version"
+require_relative "hungry_vegan/version"
+require_relative "hungry_vegan/api"
+require_relative "hungry_vegan/cli"
+require_relative "hungry_vegan/restaurant"
+
+require "http"
+require "pry"
+require "dotenv/load"
+
 
 module HungryVegan
   class Error < StandardError; end
