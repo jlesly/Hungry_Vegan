9203ca4c43a09ab5f43724f3be941b5a8aa4b81c
Lesly <leslyj2@gmail.com>
2020-12-29 11:38:21 -0500

    building cli class

--- lib/hungry_vegan/cli.rb
+++ lib/hungry_vegan/cli.rb
@@ -4,6 +4,22 @@ module HungryVegan
 
         def initialize
         end
+
+        def call
+            puts "\nWelcome to Hungry Vegan!\n"
+            puts "\nType in your 5-digit zip code to get a list of restaurants in your area.\n"
+        end 
+
+        def input 
+        end
         
+        def invalid_entry
+        end
+
+        def exit 
+        end 
+
+        def main_menu_option
+        end 
     end
 end
---
+++ lib/hungry_vegan/restaurant.rb
@@ -0,0 +1,9 @@
+module HungryVegan
+    class Restaurant 
+        attr_accessor :id, :name, :address, :rating, :phone_number, :url, :website
+
+        @@all = []
+
+        
+    end 
+end
