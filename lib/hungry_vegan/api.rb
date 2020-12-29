6db0b1deab1c8a8654560e112f09924d88a3b131
Lesly <leslyj2@gmail.com>
2020-12-29 11:30:58 -0500

    set up api

---
+++ lib/hungry_vegan/api.rb
@@ -0,0 +1,21 @@
+module HungryVegan
+    class Api 
+
+        API_HOST = "https://api.yelp.com"
+        SEARCH_PATH = "/v3/businesses/search"
+        BUSINESS_PATH = "/v3/businesses/"
+        SEARCH_LIMIT = 20
+
+        def self.search(term, location)
+            url = "#{API_HOST}#{SEARCH_PATH}"
+            params = {
+                term: term, 
+                location: location, 
+                limit: SEARCH_LIMIT,
+                sort_by: "rating"
+            }
+            response = HTTP.auth("Bearer #{ENV['YELP_API_KEY']}").get(url, params: params)
+            response.parse
+        end 
+    end 
+end 
---
+++ lib/hungry_vegan/cli.rb
@@ -0,0 +1,9 @@
+module HungryVegan
+    class Cli
+        attr_accessor = 
+
+        def initialize
+        end
+        
+    end
+end
